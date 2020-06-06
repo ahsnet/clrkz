022e565b546accbec8ee8279eb603a40dea8a7ad
ahsnet <66269973+ahsnet@users.noreply.github.com>
2020-06-06 23:31:20 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -2,7 +2,7 @@
 #About this script
 
 clear
-echo -e "Script Auto Install SSH & OpenVPN v2.0" | lolcat 
+echo -e "Script Auto Install SSH & OpenVPN" | lolcat 
 echo -e ""
 echo -e "For Debian 7 32 bit & 64 bit"
 echo -e "For VPS with KVM and VMWare virtualization"
@@ -12,9 +12,7 @@ echo -e "* Fornesia"
 echo -e "* Rzengineer"
 echo -e "* Fawzya"
 echo -e ""
-echo -e "Mod by Clrkz"
+echo -e "Mod by Ahs"
 echo -e ""
-echo -e "https://www.facebook.com/143Clarkz"
-echo -e "https://www.facebook.com/ByteHAX"
-echo -e "http://ByteHAX.blogspot.com"
+echo -e "untuk debian9"
 echo -e ""
