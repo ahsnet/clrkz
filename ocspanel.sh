470b6a6a9b49cc6b330d30bb431a8d55252cb146
ahsnet <66269973+ahsnet@users.noreply.github.com>
2020-06-15 18:07:23 +0700

    Update ocspanel.sh

--- ocspanel.sh
+++ ocspanel.sh
@@ -97,9 +97,9 @@ service nginx restart
 
 apt-get -y install zip unzip
 cd /home/vps/public_html
-wget $source/OCS.zip
-unzip OCS.zip
-rm -f OCS.zip
+wget $source/OCS_FNS.zip
+unzip OCS_FNS.zip
+rm -f OCS_FNS.zip
 chown -R www-data:www-data /home/vps/public_html
 chmod -R g+rw /home/vps/public_html
 
